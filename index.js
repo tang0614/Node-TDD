5971266b30f6e7575fc7f44313e87b372f9de191
tang0614 <tang0614@bu.edu>
2021-03-13 19:06:24 -0500

    add flow

--- index.js
+++ index.js
@@ -1,2 +1,4 @@
 const app = require('/app');
-app.listen(3000, ()=>console.log('server is listening on 3100 successfully'));
+app.listen(3000, () =>
+    console.log('server is listening on 3100 successfully')
+);
