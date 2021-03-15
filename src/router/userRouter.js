67f3424deef7309eaa0cea3435cfcf251a3a84af
tang0614 <tang0614@bu.edu>
2021-03-15 07:50:58 -0400

    add user validation

--- src/router/userRouter.js
+++ src/router/userRouter.js
@@ -4,6 +4,15 @@ const router = express.Router();
 const userService = require('../service/userService');
 
 router.post('/', async (req, res) => {
+    const user = req.body;
+    if (user.username === null) {
+        return res.status(400).send({
+            validationErrors: {
+                username: 'Username cannot be null'
+            }
+        });
+    }
+
     await userService.save(req.body);
 
     return res.send({ message: 'User created' });
