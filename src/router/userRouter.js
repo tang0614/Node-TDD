b27936a31cd6fc4bfde6eb78351aaaa286a82d9a
tang0614 <tang0614@bu.edu>
2021-03-17 05:53:35 -0400

    add other dynamic validation

--- src/router/userRouter.js
+++ src/router/userRouter.js
@@ -29,11 +29,27 @@ router.post(
     '/',
     check('username')
         .notEmpty()
-        .withMessage('Username cannot be null'),
+        .withMessage('Username cannot be null')
+        .bail()
+        .isLength({ min: 4, max: 32 })
+        .withMessage('Must have min 4 and max 32 characters'),
+    check('email')
+        .notEmpty()
+        .withMessage('E-mail cannot be null')
+        .bail()
+        .isEmail()
+        .withMessage('E-mail is not valid'),
     check('password')
         .notEmpty()
-        .withMessage('Password cannot be null'),
-    check('email').notEmpty().withMessage('E-mail cannot be null'),
+        .withMessage('Password cannot be null')
+        .bail()
+        .isLength({ min: 6 })
+        .withMessage('Password must be at least 6 characters')
+        .bail()
+        .matches(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).*$/)
+        .withMessage(
+            'Password must have at least 1 uppercase, 1 lowercase letter and 1 number'
+        ),
     async (req, res) => {
         const returned_errors = validationResult(req);
         if (!returned_errors.isEmpty()) {
